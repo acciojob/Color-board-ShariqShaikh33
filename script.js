22362084fffceeb213659e13f862a5fe82d898ab
Shariq Shaikh <shariqshaikh2705@gmail.com>
2025-04-16 21:42:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,26 +4,26 @@ const container = document.getElementById("container");
 let count = 800;
 for(let i=0;i<count;i++){
 	let makeSquare = document.createElement('div');
-	makeSquare.style.backgroundColor = 'rgb(29, 29, 29)';
+	// makeSquare.style.backgroundColor = 'rgb(29, 29, 29)';
 	makeSquare.className = ("square");
 	container.appendChild(makeSquare);
 }
 let elementArray = [...document.querySelectorAll('.square')];
-elementArray.forEach((e)=>{
-	e.addEventListener("mouseover",()=>{
-		let r = Math.floor(Math.random()*(255-0+1))+0;
-		let g = Math.floor(Math.random()*(255-0+1))+0;
-		let b = Math.floor(Math.random()*(255-0+1))+0;
-		let col = "rgb(" + r + "," + g + "," + b + ")";
-		e.style.boxShadow =  "0 0 5px "+col;
-		e.style.backgroundColor = col;
-	})
-})
+// elementArray.forEach((e)=>{
+// 	e.addEventListener("mouseover",()=>{
+// 		let r = Math.floor(Math.random()*(255-0+1))+0;
+// 		let g = Math.floor(Math.random()*(255-0+1))+0;
+// 		let b = Math.floor(Math.random()*(255-0+1))+0;
+// 		let col = "rgb(" + r + "," + g + "," + b + ")";
+// 		e.style.boxShadow =  "0 0 5px "+col;
+// 		e.style.backgroundColor = col;
+// 	})
+// })
 
-elementArray.forEach((e)=>{
-	e.addEventListener("mouseleave",()=>{
-		e.style.backgroundColor = 'rgb(29, 29, 29)';
-		e.style.transition = "1s"
-		e.style.boxShadow = "none"
-	})
-})
+// elementArray.forEach((e)=>{
+// 	e.addEventListener("mouseleave",()=>{
+// 		e.style.backgroundColor = 'rgb(29, 29, 29)';
+// 		e.style.transition = "1s"
+// 		e.style.boxShadow = "none"
+// 	})
+// })
