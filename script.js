f4f2e1f98711db5386bf27f88ded323e26ad321a
Shariq Shaikh <shariqshaikh2705@gmail.com>
2025-04-16 20:55:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+const container = document.getElementById("container");
+
+let count = 800;
+for(let i=0;i<count;i++){
+	let makeSquare = document.createElement('div');
+	
+	makeSquare.className = ("square");
+	
+	container.appendChild(makeSquare);
+}
+let elementArray = [...document.querySelectorAll('.square')];
+elementArray.forEach((e)=>{
+	e.addEventListener("mouseover",()=>{
+		let r = Math.floor(Math.random()*(255-0+1))+0;
+		let g = Math.floor(Math.random()*(255-0+1))+0;
+		let b = Math.floor(Math.random()*(255-0+1))+0;
+		let col = "rgb(" + r + "," + g + "," + b + ")";
+		e.style.boxShadow =  "0 0 5px "+col;
+
+		e.style.backgroundColor = col;
+	})
+})
+
+// let square = document.querySelectorAll(".square");
+// console.log(square)
+elementArray.forEach((e)=>{
+	e.addEventListener("mouseleave",()=>{
+		e.style.background = "#363737"
+		e.style.boxShadow = "none"
+		e.style.transition = "1s";
+	})
+})
